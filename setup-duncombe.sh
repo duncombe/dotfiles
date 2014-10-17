2bcf507f33301c5d1bf37667fd35f8a1342e24fe
Christopher Duncombe Rae <christopher.duncombe.rae@noaa.gov>
2014-10-17 19:24:15 +0000

    Creating a setup script.

---
+++ setup-duncombe.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+ln -sb ~/dotfiles/duncombe/bashrc ~/.bashrc 
+ln -sb ~/dotfiles/duncombe/bash_logout .bash_logout
+ln -sb ~/dotfiles/duncombe/bash_profile .bash_profile
+ln -sb ~/dotfiles/duncombe/bashrc_custom .bashrc_custom
+ln -sb ~/dotfiles/duncombe/vimrc .vimrc
+
+## not required
+# ln -sb ~/dotfiles/duncombe/fetchmailrc .fetchmailrc
+# ln -sb ~/dotfiles/duncombe/muttrc .muttrc
+# ln -sb ~/dotfiles/duncombe/procmailrc .procmailrc
