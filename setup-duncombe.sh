a2b3bbcf0bb68cdc810a9ab014ae4b544d4497bc
Chris Duncombe Rae <christopher.duncombe.rae@gmail.com>
2014-10-22 19:48:44 -0400

    Updating setup-duncombe.sh and gitconfig.

--- setup-duncombe.sh
+++ setup-duncombe.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+# execute this from the top directory of the dotfiles repository
+INSTALLDIR=`/bin/pwd`
+[ ! -e $INSTALLDIR/duncombe/bashrc ] && {
+	echo "dotfiles repository does not appear to be here ($INSTALLDIR)" 
+	echo exit 1
+	}
 
 for f in bashrc bash_logout bash_profile bashrc_custom vimrc ; do
-	[ ! -e ~/.${f}\~ ] &&  ln -sb ~/dotfiles/duncombe/${f} ~/.${f}  || echo  ~/.${f}\~ exists.
+	[ ! -e ~/.${f}\~ ] &&
+		ln -sb "$INSTALLDIR/duncombe/${f}" ~/.${f}  ||
+			echo  ~/.${f}\~ exists.
 done
 
 
 ## not required
+# ln -sb ~/dotfiles/duncombe/gitconfig .gitconfig
 # ln -sb ~/dotfiles/duncombe/fetchmailrc .fetchmailrc
 # ln -sb ~/dotfiles/duncombe/muttrc .muttrc
 # ln -sb ~/dotfiles/duncombe/procmailrc .procmailrc
